6eaecc0554d2e41e595d1947eb9fb1366ff53846
Yusuke.Yoshida <yy071ksu@yahoo.co.jp>
2018-07-06 15:31:38 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,6 +93,12 @@
                 @extends('layouts.app')
 
                 @section('content')
+
+                    @if (Auth::check())
+                        <?php $user = Auth::user(); ?>
+                        {{ $user->name }}
+                    @else
+
                     <div class="center jumbotron">
                         <div class="text-center">
                             <h1>Welcome to the Tasklists</h1>
