b0e7c111a83639b3c0e81f4a244cd6fdf9a6a7a7
Yusuke.Yoshida <yy071ksu@yahoo.co.jp>
2018-07-06 16:03:04 +0000

    user pages

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User; // 追加
+
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+
+        return view('users.index', [
+            'users' => $users,
+        ]);
+    
+    }
+    
+    public function index()
+    {
+        $users = User::paginate(10);
+
+        return view('users.index', [
+            'users' => $users,
+        ]);
+    }
+    
+    public function show($id)
+    {
+        $user = User::find($id);
+
+        return view('users.show', [
+            'user' => $user,
+        ]);
+    }
+
+
+}
+
---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,42 @@
+<header>
+    <nav class="navbar navbar-inverse navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/">KadaiTasklist</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                
+                <ul class="nav navbar-nav navbar-right">
+                    
+                    @if (Auth::check())
+                    
+                        <li>{!! link_to_route('users.index', 'Users') !!}</li>
+                        <li><a href="#">Users</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li>{!! link_to_route('users.show', 'My profile', ['id' => Auth::id()]) !!}</li>
+                                    <li role="separator" class="divider"></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
+                                </ul>
+                        </li>
+    
+                    @else
+                        <li>{!! link_to_route('tasks.create', '新規メッセージの投稿') !!}</li>
+                    
+                        <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
+                        <li>{!! link_to_route('login', 'Login') !!}</li>
+                    @endif    
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
